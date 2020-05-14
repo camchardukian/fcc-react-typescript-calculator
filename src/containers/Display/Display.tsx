007684c21d74ba1d7ac95efcf8bb7be7daa5e012
Cameron Chardukian <camchardukian@gmail.com>
2020-05-14 21:26:52 +0700

    feature/display fixed some TS warnings but still used the any type too many times and the app lacks any functionality

--- src/containers/Display/Display.tsx
+++ src/containers/Display/Display.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import useNumberInfo from '../../providers/NumbersProvider';
 import './styles.scss'
 
@@ -6,17 +6,13 @@ interface iDisplayProps {
   value: number;
 }
 
-// console.log('hhhh', useNumberInfo);
-
-const { number, setNumber } = useNumberInfo()!;
-
 // Documentation link: https://www.carlrippon.com/react-context-with-typescript-p2/
 // Demo code: https://codesandbox.io/s/react-ts-complex-context-function-f1cv4?fontsize=14&hidenavigation=1&theme=dark&file=/src/index.tsx
 
 const Display: React.FC<iDisplayProps> = ({ value }) => {
-  // console.log('jjjjj', UseNumberInfo)
-  // const { theme, setTheme } = useTheme()!
-  console.log('numberrrr', number)
+  const { number, setNumber }: any = useNumberInfo(''); // do not know why an argument is required here.
+  console.log('ffff', number) // cannot understand why this is undefined.
+
   return (
     <h2 id="display">{number}</h2>
   )
--- src/providers/NumbersProvider.tsx
+++ src/providers/NumbersProvider.tsx
@@ -5,31 +5,21 @@ interface iContext {
   setNumber: any,
   children?: React.ReactNode;
 }
+// Is the entire above interface useless?
 
 // @TODO try to finish implementing TypeScript with React Context and React Hooks
 
 export const NumberContext = React.createContext<iContext | undefined>(undefined);
 
 
-const NumbersProvider = (props: { children?: object, }) => {
-  const [number, setNumber]: any = useState(111);
-  // const [storedNumber, setStoredNumber] = useState(0);
-  // const [enteringNumber, setEnteringNumber] = useState(0);
-  // const [operatorType, setOperatorType] = useState('');
-
-
-  // const handleSetDisplayValue = (num: number) => {
-
-  // };
-
-
-
+const NumbersProvider = (props: any) => {
+  const [number, setNumber]: [number, any] = useState<number>(333);
+  // not sure how to change any inside the above tuple without causing another error.
 
 
   return (
     <NumberContext.Provider
       value={{
-        // handleSetDisplayValue,
         number,
         setNumber,
       }}
@@ -40,7 +30,7 @@ const NumbersProvider = (props: { children?: object, }) => {
 };
 
 export const useNumberInfo = () => React.useContext(NumberContext);
-
+//
 
 
 export default NumbersProvider;
