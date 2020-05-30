112f3ff64bb01813cfd1dc5fe9a4db58668c4273
Cameron Chardukian <camchardukian@gmail.com>
2020-05-30 19:06:02 +0700

    fix/decimal-point-zero-bug

--- src/containers/Display/Display.tsx
+++ src/containers/Display/Display.tsx
@@ -1,16 +1,14 @@
-import React, { useContext } from 'react';
-import { NumberContext } from '../../providers/NumbersProvider';
-import './styles.scss'
-
+import React, { useContext } from "react";
+import { NumberContext } from "../../providers/NumbersProvider";
+import "./styles.scss";
 
 const Display: React.FC = () => {
-
   const { enteringNumber } = useContext(NumberContext);
   return (
     <>
       <h2 id="display">{enteringNumber}</h2>
     </>
-  )
-}
+  );
+};
 
 export default Display;
